e3af907e2b475b35b4b109ad84d61eec23fcc321
rolczynski <rafal.rolczynski@gmail.com>
2020-02-24 18:44:11 +0100

    Add core datasets

---
+++ aspect_based_sentiment_analysis/datasets.py
@@ -0,0 +1,66 @@
+from abc import ABC
+from abc import abstractmethod
+from typing import Iterable
+from typing import Iterator
+from typing import List
+
+import numpy as np
+
+from .data_types import Example
+
+
+class TrainBatch:
+    """ """
+
+
+class Dataset(ABC):
+
+    @abstractmethod
+    def __iter__(self) -> Iterable[TrainBatch]:
+        """ We use datasets in our custom routines, where is a simple for loop,
+        therefore exclusively an iterable object is required. """
+
+    @abstractmethod
+    def preprocess_batch(self, batch_examples: List[Example]) -> TrainBatch:
+        """ """
+
+
+class InMemoryDataset(Dataset):
+    examples: List
+    batch_size: int
+
+    def __iter__(self) -> Iterator[TrainBatch]:
+        """ The method shuffles examples for next epoch, and returns the full
+        batches in each iteration. """
+        order = np.random.permutation(len(self.examples))
+        batch_examples = []
+        for index in order:
+            example = self.examples[index]
+            batch_examples.append(example)
+            if len(batch_examples) == self.batch_size:
+                batch = self.preprocess_batch(batch_examples)
+                yield batch
+                batch_examples = []
+
+    @abstractmethod
+    def preprocess_batch(self, batch_examples: List[Example]) -> TrainBatch:
+        """ """
+
+
+class StreamDataset(Dataset):
+    batch_size: int
+
+    def __iter__(self) -> Iterator[TrainBatch]:
+        """ """
+        examples = self.examples_generator()
+        batch_examples = []
+        for example in examples:
+            batch_examples.append(example)
+            if len(batch_examples) == self.batch_size:
+                batch_input = self.preprocess_batch(batch_examples)
+                yield batch_input
+                batch_examples = []
+
+    @abstractmethod
+    def examples_generator(self) -> Iterable[Example]:
+        """ """
