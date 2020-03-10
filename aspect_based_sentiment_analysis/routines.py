18404d49310b804dd0336d0c5ccdc537cda6e2e6
rolczynski <rafal.rolczynski@gmail.com>
2020-03-10 20:06:50 +0100

    Distill routines; add strategy; add stoptraining exception

--- aspect_based_sentiment_analysis/routines.py
+++ aspect_based_sentiment_analysis/routines.py
@@ -1,135 +1,76 @@
+import dataclasses
+import logging
 from typing import Callable
 from typing import Iterable
 from typing import List
 
 import numpy as np
 import tensorflow as tf
-import transformers
 
-from . import losses
+from .datasets import TrainBatch
 from .callbacks import Callback
 from .callbacks import CallbackList
-from .models import BertABSClassifier
-from .preprocessing.language_model_input import LanguageModelTrainBatch
-from .preprocessing.extractor_model_input import ExtractorTrainBatch
-from .preprocessing.classifier_model_input import ClassifierTrainBatch
+
+logger = logging.getLogger('absa.routines')
 
 
-def train(train_step: Callable,
-          train_dataset: Iterable,
+class StopTraining(Exception):
+    """ Exception raised when the training is forced to stop. """
+
+
+def train(strategy: tf.distribute.Strategy,
+          train_step: Callable,
+          train_dataset: Iterable[TrainBatch],
           test_step: Callable = None,
-          test_dataset: Iterable = None,
+          test_dataset: Iterable[TrainBatch] = None,
           epochs: int = 10,
-          callbacks: List = None):
+          callbacks: List[Callback] = None):
     callbacks = CallbackList(callbacks if callbacks else [])
-    for epoch in np.arange(1, epochs+1):
-        callbacks.on_epoch_begin(epoch)
-        train_loop(train_step, train_dataset, callbacks)
-        if test_step and test_dataset:
-            test_loop(test_step, test_dataset, callbacks)
-        callbacks.on_epoch_end(epoch)
+    try:
+        for epoch in np.arange(1, epochs+1):
+            callbacks.on_epoch_begin(epoch)
+            train_loop(train_step, train_dataset, callbacks, strategy)
+            if test_step and test_dataset:
+                test_loop(test_step, test_dataset, callbacks, strategy)
+            callbacks.on_epoch_end(epoch)
+    except StopTraining:
+        logger.info('The training routine is stopped.')
 
 
-def train_loop(train_step: Callable, dataset: Iterable, callbacks: Callback):
+def train_loop(train_step: Callable,
+               dataset: Iterable[TrainBatch],
+               callbacks: Callback,
+               strategy: tf.distribute.Strategy):
+    step = wrap_step_into_strategy(train_step, strategy)
     for i, batch in enumerate(dataset):
-        train_step_outputs = train_step(batch)
+        tf_batch = dataclasses.astuple(batch)
+        train_step_outputs = step(tf_batch)
         callbacks.on_train_batch_end(i, batch, *train_step_outputs)
 
 
-def test_loop(test_step: Callable, dataset: Iterable, callbacks: Callback):
+def test_loop(test_step: Callable,
+              dataset: Iterable[TrainBatch],
+              callbacks: Callback,
+              strategy: tf.distribute.Strategy):
+    step = wrap_step_into_strategy(test_step, strategy)
     for i, batch in enumerate(dataset):
-        test_step_outputs = test_step(batch)
+        tf_batch = dataclasses.astuple(batch)
+        test_step_outputs = step(tf_batch)
         callbacks.on_test_batch_end(i, batch, *test_step_outputs)
 
 
-def post_train(model: transformers.TFBertForPreTraining,
-               optimizer: tf.keras.optimizers.Optimizer,
-               train_dataset: Iterable[LanguageModelTrainBatch],
-               epochs: int,
-               test_dataset: Iterable[LanguageModelTrainBatch] = None,
-               callbacks: List[Callback] = None):
-    """ Post train (fine-tune) the pretrained language model. """
-    def train_step(batch: LanguageModelTrainBatch):
-        with tf.GradientTape() as tape:
-            model_outputs = model.call(batch.token_ids,
-                                       attention_mask=batch.attention_mask,
-                                       token_type_ids=batch.token_type_ids,
-                                       training=True)
-            loss_value = losses.language_model_loss(...)
+def wrap_step_into_strategy(step: Callable, strategy: tf.distribute.Strategy):
+    """ """
+    def one_device(batch):
+        return strategy.experimental_run_v2(step, args=batch)
 
-        variables = model.trainable_variables
-        grads = tape.gradient(loss_value, variables)
-        optimizer.apply_gradients(zip(grads, variables))
-        return [loss_value, *model_outputs]
+    def distributed(batch):
+        dataset = tf.data.Dataset.from_tensors(batch)
+        dist_batch, = strategy.experimental_distribute_dataset(dataset)
+        per_replica_outputs = strategy.experimental_run_v2(step, args=dist_batch)
+        with tf.device('CPU'):
+            return [tf.concat(per_replica_output.values, axis=0)
+                    for per_replica_output in per_replica_outputs]
 
-    def test_step(batch: LanguageModelTrainBatch):
-        model_outputs = model.call(batch.token_ids,
-                                   attention_mask=batch.attention_mask,
-                                   token_type_ids=batch.token_type_ids)
-        loss_value = losses.language_model_loss(...)
-        return [loss_value, *model_outputs]
-
-    train(train_step, train_dataset, test_step, test_dataset, epochs, callbacks)
-
-
-def tune_extractor(model: BertABSClassifier,
-                   optimizer: tf.keras.optimizers.Optimizer,
-                   train_dataset: Iterable[ExtractorTrainBatch],
-                   epochs: int,
-                   test_dataset: Iterable[ExtractorTrainBatch] = None,
-                   callbacks: List[Callback] = None):
-    """ This routines tune the extractor along with the language model. """
-    def train_step(batch: ExtractorTrainBatch):
-        with tf.GradientTape() as tape:
-            model_outputs = model.call_extractor(batch.token_ids,
-                                                 attention_mask=batch.attention_mask,
-                                                 training=True)
-            logits, *details = model_outputs
-            loss_value = losses.extractor_loss(...)
-
-        variables = model.language_model.bert.trainable_variables \
-                    + model.extractor.trainable_variables
-        grads = tape.gradient(loss_value, variables)
-        optimizer.apply_gradients(zip(grads, variables))
-        return [loss_value, *model_outputs]
-
-    def test_step(batch: ExtractorTrainBatch):
-        model_outputs = model.call_extractor(batch.token_ids,
-                                             attention_mask=batch.attention_mask)
-        loss_value = losses.extractor_loss(...)
-        return [loss_value, *model_outputs]
-
-    train(train_step, train_dataset, test_step, test_dataset, epochs, callbacks)
-
-
-def tune_classifier(model: BertABSClassifier,
-                    optimizer: tf.keras.optimizers.Optimizer,
-                    train_dataset: Iterable[ClassifierTrainBatch],
-                    epochs: int,
-                    test_dataset: Iterable[ClassifierTrainBatch] = None,
-                    callbacks: List[Callback] = None):
-    """ This routines tune the classifier along with the language model. """
-    def train_step(batch: ClassifierTrainBatch):
-        with tf.GradientTape() as tape:
-            model_outputs = model.call_classifier(batch.token_ids,
-                                                  attention_mask=batch.attention_mask,
-                                                  token_type_ids=batch.token_type_ids,
-                                                  training=True)
-            logits, *details = model_outputs
-            loss_value = losses.classifier_loss(batch.target_labels, logits)
-
-        variables = model.language_model.bert.trainable_variables \
-                    + model.classifier.trainable_variables
-        grads = tape.gradient(loss_value, variables)
-        optimizer.apply_gradients(zip(grads, variables))
-        return [loss_value, *model_outputs]
-
-    def test_step(batch: ClassifierTrainBatch):
-        model_outputs = model.call_classifier(batch.token_ids,
-                                              attention_mask=batch.attention_mask,
-                                              token_type_ids=batch.token_type_ids)
-        logits, *details = model_outputs
-        loss_value = losses.classifier_loss(batch.target_labels, logits)
-        return [loss_value, *model_outputs]
-
-    train(train_step, train_dataset, test_step, test_dataset, epochs, callbacks)
+    is_distributed = isinstance(strategy, tf.distribute.MirroredStrategy)
+    return distributed if is_distributed else one_device
