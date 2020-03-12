6f77781139005fd23a8d79fcf6dc56a5fff327eb
rolczynski <rafal.rolczynski@gmail.com>
2020-03-12 15:48:29 +0100

    Logger callback uses root logger

--- aspect_based_sentiment_analysis/callbacks.py
+++ aspect_based_sentiment_analysis/callbacks.py
@@ -58,17 +58,18 @@ class Logger(Callback):
     msg_format: str = '%(asctime)s [%(levelname)-6s] [%(name)-10s] %(message)s'
 
     def __post_init__(self):
-        logger.setLevel(self.level)
-        logger.propagate = False
+        root_logger = logging.getLogger('absa')
+        root_logger.setLevel(self.level)
+        root_logger.propagate = False
         formatter = logging.Formatter(self.msg_format, datefmt='%Y-%m-%d %H:%M:%S')
         console = logging.StreamHandler()
         console.setFormatter(formatter)
         # Handle all messages from the logger (not set the handler level)
-        logger.addHandler(console)
+        root_logger.addHandler(console)
         if self.file_path:
             file_handler = logging.FileHandler(self.file_path, mode='w')
             file_handler.setFormatter(formatter)
-            logger.addHandler(file_handler)
+            root_logger.addHandler(file_handler)
 
 
 @dataclass
