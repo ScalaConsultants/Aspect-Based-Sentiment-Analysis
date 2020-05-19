6918ded804820cef4a5512b9c4ca48a87e49577e
rolczynski <rafal.rolczynski@gmail.com>
2020-05-19 21:01:43 +0200

    Add inference performance test

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -4,6 +4,28 @@ import aspect_based_sentiment_analysis as absa
 from aspect_based_sentiment_analysis.training import ConfusionMatrix
 
 
+@pytest.fixture
+def nlp() -> absa.Pipeline:
+    sentencier = absa.sentencizer()
+    recognizer = absa.probing.AttentionGradientProduct()
+    nlp = absa.load(text_splitter=sentencier, pattern_recognizer=recognizer)
+    return nlp
+
+
+@pytest.mark.slow
+@pytest.mark.timeout(25)  # First 10s requires a pipeline to initialize.
+def test_inference(nlp: absa.Pipeline):
+    text = ("My wife and I and our 4 year old daughter stopped here "
+            "Friday-Sunday. We arrived about midday and there was a queue to "
+            "check in, which seemed to take ages to go down. Our check is was "
+            "fairly quick but not sure why others in front of us took so "
+            "long. Our room was \"ready\" although the sofa bed for our "
+            "daughter hadn't been made.")
+    aspects = ["reception", "bed", "service", "staff",
+               "location", "public", "breakfast"]
+    nlp(text, aspects)
+
+
 @pytest.mark.slow
 def test_semeval_classification_restaurants():
     examples = absa.load_examples(dataset='semeval',
